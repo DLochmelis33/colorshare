350cbc225330b92b6b1b340728a1fff8f88e5a7b
GlebSolovev <lionyoung@mail.ru>
2021-03-20 01:54:39 +0300

    Add file selection and improve text view

--- app/src/main/java/ru/hse/colorshare/MainActivity.java
+++ app/src/main/java/ru/hse/colorshare/MainActivity.java
@@ -1,23 +1,39 @@
 package ru.hse.colorshare;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView fileToSendTextView;
+    private String fileToSendUri;
+    private String fileToSendName;
+    private long fileToSendSize;
+
+    private static final int PICK_FILE_REQUEST = 2;
+    private static final String LOG_TAG = "ColorShareLogTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +44,35 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar); // currently unused
 
         fileToSendTextView = findViewById(R.id.fileToSendTextView);
+        fileToSendName = getResources().getString(R.string.file_to_send);
     }
 
     public void onClickSelectFile(View view) {
-        Toast.makeText(getApplicationContext(), "Select your file", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "Attempt to select a file");
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(intent, PICK_FILE_REQUEST);
+        } catch (ActivityNotFoundException ex) {
+            Toast.makeText(getApplicationContext(), "File Manager not found", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onClickStartSending(View view) {
+        if (fileToSendUri == null) {
+            Toast.makeText(getApplicationContext(), "Select a file", Toast.LENGTH_SHORT).show();
+            return;
+        }
         Toast.makeText(getApplicationContext(), "Start sending...", Toast.LENGTH_SHORT).show();
+        // work with file
+//        try (InputStream inputStream =
+//                     getContentResolver().openInputStream(uri); BufferedReader reader = new BufferedReader(
+//                new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+//            // read from input stream
+//        } catch (IOException exc) {
+//            Toast.makeText(getApplicationContext(), "File reading failed, try again", Toast.LENGTH_SHORT).show();
+//        }
     }
 
     public void onClickTest(View view) {
@@ -46,5 +83,62 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent resultData) {
+        Log.d(LOG_TAG, "requestCode = " + requestCode + ", resultCode = " + resultCode);
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        switch (requestCode) {
+            case PICK_FILE_REQUEST:
+                if (resultData == null) {
+                    Toast.makeText(getApplicationContext(), "File selection failed, try again", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                Uri uri = resultData.getData();
+                fileToSendUri = uri.toString();
+                Log.d(LOG_TAG, "File uri: " + fileToSendUri);
+
+                // get file name and size
+                Cursor cursor =
+                        getContentResolver().query(uri, null, null, null, null);
+                try {
+                    int nameIndex = cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME);
+                    int sizeIndex = cursor.getColumnIndexOrThrow(OpenableColumns.SIZE);
+                    cursor.moveToFirst();
+                    fileToSendName = cursor.getString(nameIndex);
+                    fileToSendSize = cursor.getLong(sizeIndex);
+                } catch (IllegalArgumentException exc) {
+                    Toast.makeText(getApplicationContext(), "Failed to get file info, try again", Toast.LENGTH_SHORT).show();
+                    fileToSendUri = null;
+                    break;
+                }
+                cursor.close();
+                Log.d(LOG_TAG, "File info: " + "name = " + fileToSendName + ", size = " + fileToSendSize);
+
+                fileToSendTextView.setText(fileToSendName);
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, resultData);
+    }
+
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("fileToSendUri", fileToSendUri);
+        outState.putString("fileToSendName", fileToSendName);
+        outState.putLong("fileToSendSize", fileToSendSize);
+        Log.d(LOG_TAG, "onSaveInstanceState");
+    }
+
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        fileToSendUri = savedInstanceState.getString("fileToSendUri");
+        fileToSendName = savedInstanceState.getString("fileToSendName");
+        fileToSendSize = savedInstanceState.getLong("fileToSendSize");
+        fileToSendTextView.setText(fileToSendName);
+        Log.d(LOG_TAG, "onRestoreInstanceState");
     }
 }
