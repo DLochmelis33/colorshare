920345f5ecee54ef627ea3dcdca8cd7194ce0ea5
kibrq <ki.br178@gmail.com>
2021-05-19 01:59:28 +0300

    Move color choosing into interface

--- app/src/main/java/ru/hse/colorshare/coding/decoding/impl/FourColorsDataFrameDecoder.java
+++ app/src/main/java/ru/hse/colorshare/coding/decoding/impl/FourColorsDataFrameDecoder.java
@@ -1,14 +1,11 @@
 package ru.hse.colorshare.coding.decoding.impl;
 
-import android.graphics.Color;
-
 import java.util.Objects;
 import java.util.zip.Checksum;
 
 import ru.hse.colorshare.coding.decoding.ByteDataFrame;
 import ru.hse.colorshare.coding.decoding.ColorDataFrameDecoder;
 
-import static ru.hse.colorshare.coding.util.FourColorsDataFrameUtil.ALL_TWO_BIT_UNITS;
 import static ru.hse.colorshare.coding.util.FourColorsDataFrameUtil.BITS_PER_UNIT;
 import static ru.hse.colorshare.coding.util.FourColorsDataFrameUtil.FROM_COLORS;
 import static ru.hse.colorshare.coding.util.FourColorsDataFrameUtil.UNITS_PER_BYTE;
@@ -16,34 +13,17 @@ import static ru.hse.colorshare.coding.util.FourColorsDataFrameUtil.UNITS_PER_BY
 public class FourColorsDataFrameDecoder implements ColorDataFrameDecoder {
 
     private final Checksum checksum;
+    private final OptimalColorChooser chooser;
 
     public FourColorsDataFrameDecoder(Checksum checksum) {
         this.checksum = checksum;
-    }
-
-    private int distance(int color1, int color2) {
-        return Math.abs(Color.red(color1) - Color.red(color2)) +
-                Math.abs(Color.green(color1) - Color.green(color2)) +
-                Math.abs(Color.blue(color1) - Color.blue(color2));
-    }
-
-    private int chooseClosestColor(int color) {
-        int currentOptimalIndex = 0;
-        int currentOptimalDistance = Integer.MAX_VALUE;
-        for (int i = 0; i < ALL_TWO_BIT_UNITS.size(); i++) {
-            int currentDistance = distance(color, ALL_TWO_BIT_UNITS.get(i).color);
-            if (currentDistance < currentOptimalDistance) {
-                currentOptimalIndex = i;
-                currentOptimalDistance = currentDistance;
-            }
-        }
-        return ALL_TWO_BIT_UNITS.get(currentOptimalIndex).color;
+        this.chooser = new FourColorManhattanMetricsColorChooser();
     }
 
     private byte readColorsAsByte(int[] colors, int offset) {
         byte result = 0;
         for (int i = 0; i < UNITS_PER_BYTE; i++) {
-            Integer bits = FROM_COLORS.get(chooseClosestColor(colors[offset + i]));
+            Integer bits = FROM_COLORS.get(chooser.chooseClosest(colors[offset + i]));
             Objects.requireNonNull(bits);
             result |= bits << i * BITS_PER_UNIT;
         }
